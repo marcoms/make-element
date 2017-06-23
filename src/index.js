76ff5adb523d476fe89d776a2f4cb4ce872c44fa
Marco Scannadinari <m@scannadinari.co.uk>
2017-06-23 19:10:46 +0100

    Remove version property

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import {version} from '../package.json';
-
 function noop() {}
 
 function identity(val) {
@@ -308,11 +306,4 @@ function makeElement(def={}) {
 	return CustomElement;
 }
 
-const [versionMajor, versionMinor, versionPatch] = version.split('.');
-makeElement.version = {
-	major: Number.parseInt(versionMajor, 10),
-	minor: Number.parseInt(versionMinor, 10),
-	patch: Number.parseInt(versionPatch, 10),
-};
-
-export default makeElement;
+module.exports = makeElement;
