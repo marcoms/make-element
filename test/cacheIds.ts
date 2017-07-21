38fdaf0435e508905adccad87a22be77c9d87c44
Marco Scannadinari <m@scannadinari.co.uk>
2017-07-22 00:26:02 +0100

    Add cacheIds tests

---
+++ test/cacheIds.ts
@@ -0,0 +1,41 @@
+import {assert} from 'chai';
+
+import me from 'src/index';
+import {customElName} from './tools';
+
+describe('cacheIds', () => {
+	it('should store elements with id attributes on the $ object', () => {
+		const El = me({
+			template: `
+				<div id="a"></div>
+				<div id="b"></div>
+				<div id="c"></div>
+			`,
+		});
+
+		customElements.define(customElName(), El);
+		const el = new El();
+
+		assert.strictEqual(el.$['a'].id, 'a');
+		assert.strictEqual(el.$['b'].id, 'b');
+		assert.strictEqual(el.$['c'].id, 'c');
+	});
+
+	it('should store elements with id attributes on the $ object, with shadow DOM enabled', () => {
+		const El = me({
+			shadowDom: true,
+			template: `
+				<div id="a"></div>
+				<div id="b"></div>
+				<div id="c"></div>
+			`,
+		});
+
+		customElements.define(customElName(), El);
+		const el = new El();
+
+		assert.strictEqual(el.$['a'].id, 'a');
+		assert.strictEqual(el.$['b'].id, 'b');
+		assert.strictEqual(el.$['c'].id, 'c');
+	});
+});
