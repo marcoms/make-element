6dbe686652f6e642000b183a4063d68f27c8b6e0
Marco Scannadinari <m@scannadinari.co.uk>
2017-07-22 00:20:43 +0100

    Add file rule for test template file

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,35 @@
 module.exports = (config) => {
-  config.set({
-    frameworks: ['mocha', 'karma-typescript'],
-    browsers: ['ChromeHeadless'],
-    singleRun: true,
-    concurrency: Infinity,
-    reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_WARN,
+	config.set({
+		frameworks: ['mocha', 'karma-typescript'],
+		browsers: ['ChromeHeadless'],
+		singleRun: true,
+		concurrency: Infinity,
+		reporters: ['mocha'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_WARN,
 
-    files: [
-      'src/*.ts',
-      'test/*.ts',
-    ],
+		files: [
+			'src/*.ts',
+			'test/*.ts',
 
-    preprocessors: {
-      '**/*.ts': ['karma-typescript'],
-    },
+			{
+				pattern: 'test/*.html',
+				watched: false,
+				included: false,
+				served: true,
+			},
+		],
 
-    karmaTypescriptConfig: {
-      compilerOptions: {
-        target: 'es6',
-        baseUrl: __dirname,
-      },
-    },
-  })
+		preprocessors: {
+			'**/*.ts': ['karma-typescript'],
+		},
+
+		karmaTypescriptConfig: {
+			compilerOptions: {
+				target: 'es6',
+				baseUrl: __dirname,
+			},
+		},
+	});
 };
